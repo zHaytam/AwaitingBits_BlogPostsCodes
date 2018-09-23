5b3f12d9d71c0c80230b37cab6a5032d7fc77e07
zHaytam <haytam.zanid@gmail.com>
2018-09-23 19:46:22 +0100

    Generate a documentation for an ASP.NET Core Web API using Swagger

---
+++ Generate a documentation for an ASP.NET Core Web API using Swagger/Example/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Example.Controllers
+{
+    [SwaggerTag("Requests about values")]
+    [Route("api/[action]")]
+    public class ValuesController : Controller
+    {
+
+        /// <summary>
+        /// Gets two values.
+        /// </summary>
+        [HttpGet]
+        [SwaggerOperation(Summary = "Gets two values", Description = "Gets two hardcoded values")]
+        [SwaggerResponse(200, "I guess everything worked")]
+        [SwaggerResponse(400, "BAD REQUUUUEST")]
+        public IEnumerable<string> Values()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Gets a value.
+        /// </summary>
+        /// <param name="id">The id !!</param>x
+        [HttpGet]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Posts a value.
+        /// </summary>
+        /// <param name="value">The value !!</param>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// Puts a value.
+        /// </summary>
+        /// <param name="id">The id !!</param>
+        /// <param name="value">The value !!</param>
+        [HttpPut]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// Deletes an entry.
+        /// </summary>
+        /// <param name="id">The id !!</param>
+        [HttpDelete]
+        public void Delete(int id)
+        {
+        }
+
+    }
+}
---
+++ Generate a documentation for an ASP.NET Core Web API using Swagger/Example/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Example
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            // Swagger documentation
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "Example API",
+                    Version = "v1"
+                });
+
+                // XML Documentation
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+                // Annotations
+                c.EnableAnnotations();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "docs/{documentName}/docs.json";
+            });
+
+            // Swagger UI
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/docs/v1/docs.json", "Exampl API v1");
+                c.RoutePrefix = "docs";
+            });
+
+            app.UseMvc();
+        }
+
+    }
+}
